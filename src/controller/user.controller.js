8d61ca32d4c5cff051a6acc8ce3c3131db4d7b55
gotha331 <31029469+gotha331@users.noreply.github.com>
2024-05-24 19:54:04 +0800

    refactor: module splitting

---
+++ src/controller/user.controller.js
@@ -0,0 +1,12 @@
+class UserController {
+  async register(ctx, next) {
+    console.log(ctx)
+    ctx.body = '用户注册成功'
+  }
+
+  async login(ctx, next) {
+    ctx.body = '用户登录成功'
+  }
+}
+
+module.exports = new UserController()
--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,9 +1,12 @@
 const Router = require('koa-router')
+const { register, login } = require('../controller/user.controller')
 
-const router = new Router({prefix:'/users'})
+const router = new Router({ prefix: '/user' })
 
-router.get('/', (ctx, next) => {
-  ctx.body = "hello user"
-})
+// 注册接口
+router.post('/register', register)
+
+// 登录接口
+router.post('/login', login)
 
 module.exports = router
