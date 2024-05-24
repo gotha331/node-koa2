ea70690e5b726f7c1cb76a1d3d3e991677d1b19b
gotha331 <31029469+gotha331@users.noreply.github.com>
2024-05-25 00:04:31 +0800

    feat: add error handling

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,19 +1,44 @@
-const { createUser } = require('../service/user.service')
+const { createUser, getUserInfo } = require('../service/user.service')
 
 class UserController {
   async register(ctx, next) {
-    console.log(ctx)
-    console.log(ctx.request.body)
-
+    // 1.获取数据
     const { username, password } = ctx.request.body
 
-    //操作数据库
+    // 合法性
+    if (!username || !password) {
+      console.error('用户名或密码为空', ctx.request.body)
+
+      ctx.status = 400
+      ctx.body = {
+        code: '10001',
+        message: '用户名或密码为空',
+        result: ''
+      }
+      return
+    }
+
+    // 合理性
+    if (await getUserInfo({ username })) {
+      ctx.status = 409
+      ctx.body = {
+        code: '10002',
+        message: '用户已存在',
+        result: ''
+      }
+      return
+    }
+
+    // 2.操作数据库
     const res = await createUser(username, password)
 
     ctx.body = {
-      code: '0000',
-      msg: 'success',
-      data: res
+      code: 0,
+      message: '用户注册成功',
+      result: {
+        id: res.id,
+        username: res.username
+      }
     }
   }
 
